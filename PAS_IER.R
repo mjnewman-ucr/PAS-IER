9ad7e048399fb88bb118236ac29583852c549ef4
mjnewman-ucr <mnewm011@ucr.edu>
2022-03-11 17:28:08 -0800

    Started running descriptives

--- PAS_IER.R
+++ PAS_IER.R
@@ -7,6 +7,9 @@
 library("tidyverse")
 library("dplyr")
 library("readr")
+library("lubridate")
+library("Hmisc")
+library("DataExplorer")
 
 #> Before reading in the data file I did some basic cleaning (hardcoding, yikes)
 #> of the csv file. I removed some weird columns/rows:
@@ -25,9 +28,9 @@ library("readr")
 
 #READING IN DATA FILE
 
-ds <- read_csv("data/pasier_testing.csv", col_names = T, na = "9999", name_repair = tolower)
+ds <- read_csv("data/pasier_mar11_2022_2.csv", col_names = T, na = "9999", name_repair = tolower)
 colnames <- as.vector(names(ds))
-ds <- read_csv("data/pasier_testing.csv", col_names = colnames, na = "9999", skip = 15, name_repair = tolower)
+ds <- read_csv("data/pasier_mar11_2022_2.csv", col_names = colnames, na = "9999", skip = 15, name_repair = tolower)
 
 
 #-------------------------------------------------------------------------------------------------------------
@@ -45,10 +48,10 @@ glimpse(ds)
 ds <- ds %>% rename(date = recordeddate,
               duration = `duration (in seconds)`)
 
-ds <- ds %>% mutate(ID = 8001:8132)
+ds <- ds %>% mutate(ID = 8001:8194)
 
 #Putting ID first and removing unnecessary columns
-ds <- select(ds, c(ID, ierc_1a:income))
+ds <- select(ds, c(ID, date, ierc_1a:income))
 glimpse(ds)
 
 #write_csv(ds, "cleaned_data_for_john.csv")
@@ -238,6 +241,110 @@ ds$ders_clarity <- rowMeans(subset(ds, select = c(ders_1r, ders_4, ders_5, ders_
 
 ds <- ds %>% mutate(ders_total = ders_nonaccept + ders_goals + ders_impulse + ders_awareness
                     + ders_strategies + ders_clarity) 
+
+## DEMOGRAPHICS
+#For some off reason, year of birth got recoded weirdly by qualtrics.
+
+#> 1995 = 96
+#> 1996 = 97
+#> 1997 = 98
+#> 1998 = 99
+#> 1999 = 100
+#> 2000 = 101
+#> 2001 = 102
+#> 2002 = 103
+#> 2003 = 104
+#> 2004 = 105
+#> 2005 = 106
+
+#Calculating age
+ds$`age#3_1` <- recode(ds$`age#3_1`, '96' = 1995L, '97' = 1996L, '98' = 1997L, '99' = 1998L, 
+       '100' = 1999L, '101' = 2000L, '102' = 2001L, '103' = 2002L, '104' = 2003L, 
+       '105' = 2004L, '106' = 2005L)
+
+ds <- unite(ds, dob, 'age#1_1':'age#3_1', sep = "-", remove = F, na.rm = T)
+
+
+ds$date <- as_date(ds$date, format = '%d/%m/%y')
+
+ds$dob <- as_date(ds$dob, format = '%m-%d-%Y')
+
+ds$age <- as.period(interval(start = ds$dob, end = ds$date), unit = "year")
+ds$age <- str_sub(ds$age, 1, 2) 
+
+ds$age <- as.numeric(ds$age)
+
+#Tidying race... recoding responses of multiple ethnicities as 9
+ds$race <- ifelse(str_length(ds$race) > 1, 9, ds$race)
+
+
+#Creating a demographics dataset
+demographics <- select(ds, c(ID, age, gender, s_orientation, relationship, race, home, dependent:income))
+
+
+
+##-----------------------------------------------------------------------------
+##DEMOGRAPHIC DESCRIPTIVES
+
+summary(demographics)
+describe(demographics)
+
+
+ggplot(demographics, aes(x = age)) + 
+  geom_histogram() + 
+  xlab("age")
+
+ggplot(demographics, aes(x = race)) + 
+  geom_histogram() + 
+  xlab("race")
+
+ggplot(demographics, aes(x = income)) + 
+  geom_histogram() + 
+  xlab("income")
+
+rcorr(as.matrix(demographics))
+
+##-----------------------------------------------------------------------------
+## Survey Descriptives
+
+flatten_corr_matrix <- function(cormat, pmat) {
+  ut <- upper.tri(cormat)
+  data.frame(
+    row = rownames(cormat)[row(cormat)[ut]],
+    column = rownames(cormat)[col(cormat)[ut]],
+    cor  =(cormat)[ut],
+    p = pmat[ut]
+  )
+}
+
+df <- select(ds, c(ID, age, gender, s_orientation, relationship, race, home, dependent:income, 
+             iris_r:ppass_goals, sdt_autonomy:sdt_relatedness, bnsr_autonomy:cerq_otherblame,
+             ders_nonaccept:ders_total))
+
+df <- select(ds, c(iris_r:ppass_goals, sdt_autonomy:sdt_relatedness, bnsr_autonomy:cerq_otherblame,
+                   ders_nonaccept:ders_total))
+
+df <- select(ds, c(iris_r:ppass_goals))
+rcorr(as.matrix(df))
+iris_ppass <- rcorr(as.matrix(df))
+flatten_corr_matrix(iris_ppass$r, iris_ppass$P)
+
+df <- select(ds, c(iris_r:iris_pp, bnsr_autonomy:bnsr_relatedness))
+rcorr(as.matrix(df))
+iris_bnsr <- rcorr(as.matrix(df))
+flatten_corr_matrix(iris_bnsr$r, iris_bnsr$P)
+
+df <- select(ds, c(iris_r:iris_pp, ders_nonaccept:ders_total))
+rcorr(as.matrix(df))
+iris_ders <- rcorr(as.matrix(df))
+flatten_corr_matrix(iris_ders$r, iris_ders$P)
+
+df <- select(ds, c(iris_r:iris_pp, sdt_autonomy:sdt_relatedness))
+rcorr(as.matrix(df))
+iris_sdt <- rcorr(as.matrix(df))
+flatten_corr_matrix(iris_sdt$r, iris_sdt$P)
+
+
 
 
 
