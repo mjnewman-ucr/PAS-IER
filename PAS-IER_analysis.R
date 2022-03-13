91b38f01e978e047f532be76b1d596371b66a418
mjnewman-ucr <mnewm011@ucr.edu>
2022-03-12 17:28:51 -0800

    Worked on RMarkdown

--- PAS-IER_analysis.R
+++ PAS-IER_analysis.R
@@ -1,17 +1,83 @@
+##DATA ANALYSIS
+
 library("tidyverse")
 library("dplyr")
 library("readr")
 library("lubridate")
 library("Hmisc")
 library("DataExplorer")
+library("ggpubr")
+library("moments")
 
-ds <- read_csv("data/pasier_data_cleaned.csv", col_names = T, na = "9999")
+install.packages("moments")
 
+ds <- read_csv("data/pasier_data_cleaned.csv", col_names = T, na = "NA")
+summary(ds)
+glimpse(ds)
 ##-----------------------------------------------------------------------------
+##Testing assumptions
 
+ggqqplot(ds$iris_r)
+ggqqplot(ds$iris_cs)
+ggqqplot(ds$iris_h)
+ggqqplot(ds$iris_pp)
+ggqqplot(ds$ppass_as)
+ggqqplot(ds$ppass_pc)
+ggqqplot(ds$ders_total)
+ggqqplot(ds$ders_goals) 
+ggqqplot(ds$ders_awareness)
+ggqqplot(ds$cerq_reappraisal)
+ggqqplot(ds$sdt_autonomy)
+ggqqplot(ds$sdt_competence)
+ggqqplot(ds$sdt_relatedness)
+
+shapiro.test(ds$ppass_as)
+ggplot(ds, aes(x = ppass_as)) + 
+  geom_histogram(bins = 100) + 
+  xlab("autonomy support")
+skewness(ds$ppass_as, na.rm = TRUE)
+
+shapiro.test(ds$ppass_pc)
+ggplot(ds, aes(x = ppass_pc)) + 
+  geom_histogram(bins = 100) + 
+  xlab("parental control")
+skewness(ds$ppass_pc, na.rm = TRUE)
+
+
+shapiro.test(ds$iris_r)
+ggplot(ds, aes(x = iris_r)) + 
+  geom_histogram(bins = 100) + 
+  xlab("responsiveness")
+skewness(ds$iris_r, na.rm = TRUE)
+
+shapiro.test(ds$iris_cs)
+ggplot(ds, aes(x = iris_cs)) + 
+  geom_histogram(bins = 100) + 
+  xlab("cog support")
+skewness(ds$iris_cs, na.rm = TRUE)
+
+shapiro.test(ds$iris_h)
+ggplot(ds, aes(x = iris_h)) + 
+  geom_histogram(bins = 100) + 
+  xlab("hostility")
+skewness(ds$iris_h, na.rm = TRUE)
+
+shapiro.test(ds$ppass_goals)
+ggplot(ds, aes(x = ppass_goals)) + 
+  geom_histogram(bins = 100) + 
+  xlab("ppass goals")
+skewness(ds$ppass_goals, na.rm = TRUE)
+
+shapiro.test(ds$ppass_threat)
+ggplot(ds, aes(x = ppass_threat)) + 
+  geom_histogram(bins = 100) + 
+  xlab("ppass threat")
+skewness(ds$ppass_threat, na.rm = TRUE)
+
+shapiro.test(ds$ppass_guilt)
 
 #Creating a demographics dataset
-demographics <- select(ds, c(ID, age, gender, s_orientation, relationship, race, home, dependent:income))
+demographics <- select(ds, c(age, gender, s_orientation, relationship, race, home, dependent:income))
 
 
 
@@ -37,6 +103,35 @@ ggplot(demographics, aes(x = income)) +
 rcorr(as.matrix(demographics))
 
 ##-----------------------------------------------------------------------------
+
+ggplot(ds, aes(x=iris_r, y=ppass_as)) + geom_point()
+ggplot(ds, aes(x=iris_cs, y=ppass_as)) + geom_point()
+ggplot(ds, aes(x=iris_h, y=ppass_as)) + geom_point()
+ggplot(ds, aes(x=iris_pp, y=ppass_as)) + geom_point()
+ggplot(ds, aes(x=iris_r, y=ppass_pc)) + geom_point()
+ggplot(ds, aes(x=iris_cs, y=ppass_pc)) + geom_point()
+ggplot(ds, aes(x=iris_h, y=ppass_pc)) + geom_point()
+ggplot(ds, aes(x=iris_pp, y=ppass_pc)) + geom_point()
+ggplot(ds, aes(x=iris_r, y=eff_help)) + geom_point()
+ggplot(ds, aes(x=iris_cs, y=eff_help)) + geom_point()
+ggplot(ds, aes(x=iris_h, y=eff_help)) + geom_point()
+ggplot(ds, aes(x=iris_pp, y=eff_help)) + geom_point()
+ggplot(ds, aes(x=iris_h, y=eff_self)) + geom_point()
+
+#---------
+
+#Standardized
+
+df <- select(ds, c(age, gender, s_orientation, relationship, race, home, dependent:income, 
+                   eff_help:eff_control, iris_r:ppass_pc, sdt_autonomy:sdt_relatedness, 
+                   bnsr_autonomy:cerq_otherblame, ders_nonaccept:ders_total))
+
+df_scaled <- as.data.frame(scale(df))
+
+ggplot(df_scaled, aes(x=iris_r, y=eff_help)) + geom_point()
+ggplot(df_scaled, aes(x=iris_r, y=ppass_as)) + geom_point() + 
+  geom_point()+
+  geom_smooth(method=lm)
 
 
 ## Survey Descriptives
